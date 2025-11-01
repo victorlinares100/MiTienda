7e568063a111fe25696e5ac338bbe54146663917
victorlinares100 <vdlg28072004@gmail.com>
2025-10-31 22:46:52 -0300

    Correcion de diseño y orden de carpetas con los archivos

---
+++ app/src/main/java/ViewModel/ViewModelFactory.kt
@@ -0,0 +1,15 @@
+package ViewModel
+
+import Data.ProductRepository
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory(private val repository: ProductRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ProductViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
